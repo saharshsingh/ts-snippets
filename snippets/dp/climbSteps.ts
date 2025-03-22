f4ae301849b25a5ab6c956ed8e3e668d49b575ff
Saharsh Singh <saharsh.singh@gmail.com>
2025-03-22 03:14:05 -0500

    Refactor to use functions and hoisting for better readability

--- snippets/dp/climbSteps.ts
+++ snippets/dp/climbSteps.ts
@@ -1,24 +1,29 @@
+/**
+ * You are climbing a staircase with n steps. You can take either 1 or 2 steps at a time.
+ * In how many distinct ways can you climb to the top?
+ */
+const climbSteps = climbStepsIteratively;
+export default climbSteps;
+
 export function climbStepsIteratively(n: number): number {
-  const numOfWays: number[] = new Array<number>(n + 1);
-
-  numOfWays[1] = 1;
-  numOfWays[2] = 2;
-  for (let i = 3; i <= n; i++) {
-    numOfWays[i] = numOfWays[i - 1] + numOfWays[i - 2];
+  if (n <= 1) {
+    return 1;
   }
-
-  return numOfWays[n];
+  let prev = 1;
+  let current = 2;
+  for (let i = 3; i <= n; i++) {
+    const next = current + prev;
+    prev = current;
+    current = next;
+  }
+  return current;
 }
 
 export function climbStepsRecursively(n: number): number {
   return climbStepsUsingMemo(n);
 }
 
-export function climbStepsUsingMemo(n: number, memo?: Record<number, number>): number {
-  if (!memo) {
-    memo = {};
-  }
-
+function climbStepsUsingMemo(n: number, memo: Record<number, number> = {}): number {
   // Check if the result is already computed
   if (memo[n] !== undefined) {
     return memo[n];
@@ -36,10 +41,3 @@ export function climbStepsUsingMemo(n: number, memo?: Record<number, number>): n
   memo[n] = result;
   return result;
 }
-
-/**
- * You are climbing a staircase with n steps. You can take either 1 or 2 steps at a time.
- * In how many distinct ways can you climb to the top?
- */
-const climbSteps = climbStepsIteratively;
-export default climbSteps;
--- snippets/dp/longestCommonSubsequence.ts
+++ snippets/dp/longestCommonSubsequence.ts
@@ -1,14 +1,46 @@
-export const longestCommonSubsequenceRecursively = (first: string, second: string): number => {
-  return longestCommonSubsequenceUsingMemo(first, second, 0, 0, {});
-};
+/**
+ * Find longest common subsequence between two string values. A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
+ *
+ * @param first
+ * @param second
+ * @returns length of the longest subsequence
+ */
+const longestCommonSubsequence = longestCommonSubsequenceIteratively;
+export default longestCommonSubsequence;
 
-const longestCommonSubsequenceUsingMemo = (
+export function longestCommonSubsequenceIteratively(first: string, second: string): number {
+  const firstL = first.length;
+  const secondL = second.length;
+
+  const dp: number[][] = Array.from({ length: firstL + 1 }, () =>
+    new Array<number>(secondL + 1).fill(0)
+  );
+
+  for (let i = 1; i <= firstL; i++) {
+    for (let j = 1; j <= secondL; j++) {
+      if (first.charAt(i - 1) === second.charAt(j - 1)) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+        continue;
+      }
+
+      dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+    }
+  }
+
+  return dp[firstL][secondL];
+}
+
+export function longestCommonSubsequenceRecursively(first: string, second: string): number {
+  return longestCommonSubsequenceUsingMemo(first, second, 0, 0);
+}
+
+function longestCommonSubsequenceUsingMemo(
   first: string,
   second: string,
   f: number,
   s: number,
-  memo: Record<string, number>
-): number => {
+  memo: Record<string, number> = {}
+): number {
   if (f >= first.length || s >= second.length) {
     return 0;
   }
@@ -29,36 +61,4 @@ const longestCommonSubsequenceUsingMemo = (
   const result = Math.max(withoutFirst, withoutSecond);
   memo[memoKey] = result;
   return result;
-};
-
-export const longestCommonSubsequenceIteratively = (first: string, second: string): number => {
-  const firstL = first.length;
-  const secondL = second.length;
-
-  const dp: number[][] = Array.from({ length: firstL + 1 }, () =>
-    new Array<number>(secondL + 1).fill(0)
-  );
-
-  for (let i = 1; i <= firstL; i++) {
-    for (let j = 1; j <= secondL; j++) {
-      if (first.charAt(i - 1) === second.charAt(j - 1)) {
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-        continue;
-      }
-
-      dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
-    }
-  }
-
-  return dp[firstL][secondL];
-};
-
-/**
- * Find longest common subsequence between two string values. A subsequence is a sequence that can be derived from another sequence by deleting some or no elements without changing the order of the remaining elements.
- *
- * @param first
- * @param second
- * @returns length of the longest subsequence
- */
-const longestCommonSubsequence = longestCommonSubsequenceIteratively;
-export default longestCommonSubsequence;
+}
